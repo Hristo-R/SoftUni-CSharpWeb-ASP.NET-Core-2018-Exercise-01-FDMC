10c63c9ef98e52c05a3d7584630104971601fdc0
Hristo-R <hristo.razpopov@abv.bg>
2018-08-01 17:20:22 +0300

    Add project files.

---
+++ FDMC.Data/CatContext.cs
@@ -0,0 +1,19 @@
+﻿namespace FDMC.Data
+{
+    using FDMC.Models;
+    using Microsoft.EntityFrameworkCore;
+
+    public class CatContext : DbContext
+    {
+        public DbSet<Cat> Cats { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Data Source=.;Database=Cats_MVC;Integrated Security=True");
+            }
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
